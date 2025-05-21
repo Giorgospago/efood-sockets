264e0e22bff0b98757bea75359c7d7406cd97fcd
Pagonoudis <giorgospago23@gmail.com>
2025-05-21 11:27:01 +0300

    Refactor server initialization and socket handling logic in app.js

--- app.js
+++ app.js
@@ -7,48 +7,83 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const axiosInstance = require('./lib/axiosInstance');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        credentials: true
+async function main() {
+    console.log('Starting server...');
+
+    try {
+        console.log('Deleting old sockets...');
+        await axiosInstance.delete('/delete-all-sockets');
+    } catch (error) {
+        console.error('[Error]', error);
     }
-});
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected ' + socket.id);
-
-    socket.on("user-id", (data) => {
-        console.log('user-id', data);
-    });
-    
-    socket.on("driver-location", (data) => {
-        axiosInstance
-            .post('/driver-location', data)
-            .then((response) => {
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.error('[Error]', error.response.data);
-            });
+    console.log('Starting express server...');
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+            credentials: true
+        }
     });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected ' + socket.id);
-    });
-});
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-server.listen(process.env.PORT, () => {
-    console.log('listening on *:' + process.env.PORT);
-});
+
+    app.get('/', (req, res) => {
+        res.json({ message: 'Hello World!' });
+    });
+
+    io.on('connection', (socket) => {
+        console.log('a user connected ' + socket.id);
+
+        socket.on("user-id", (data) => {
+            try {
+                axiosInstance.post('/set-user-socket', {
+                    user_id: data.user_id,
+                    socket_id: socket.id
+                });
+            } catch (error) {
+                console.error('[Error]', error);
+            }
+        });
+
+        socket.on("driver-location", (data) => {
+            axiosInstance
+                .post('/driver-location', data)
+                .then((response) => {
+                    if (response.data.success === true) {
+                        for (let msg of response.data.data) {
+                            for (let socketId of msg.socket_ids) {
+                                io.to(socketId)
+                                    .emit(msg.channel, msg.data);
+                            }
+                        }
+                    }
+                })
+                .catch((error) => {
+                    console.error('[Error]', error.response.data);
+                });
+        });
+
+        socket.on('disconnect', () => {
+            try {
+                axiosInstance.delete('/delete-user-socket', {
+                    params: {
+                        socket_id: socket.id
+                    }
+                });
+            } catch (error) {
+                console.error('[Error]', error);
+            }
+        });
+    });
+
+    server.listen(process.env.PORT, () => {
+        console.log('listening on *:' + process.env.PORT);
+    });
+}
+main();
